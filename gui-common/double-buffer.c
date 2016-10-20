adf914a1016f4423a6ba87931e476bd841d2f35b
HW42 <hw42@ipsumj.de>
2016-10-20 04:01:20 +0200

    remove useless system() call in double-buffer.c

--- gui-common/double-buffer.c
+++ gui-common/double-buffer.c
@@ -53,11 +53,9 @@ void double_buffer_append(char *buf, int size)
         char *newbuf;
         if (newsize > BUFFER_SIZE_MAX) {
             fprintf(stderr,
-                    "double_buffer_append: "
+                    "double_buffer_append failed: "
                     "offset=%d, data_count=%d, req_size=%d\n",
                     data_offset, data_count, size);
-            system("xmessage -button OK:2 'Out of buffer space "
-                   "(AppVM refuses to read data?), terminating...'");
             exit(1);
         }
         newbuf = malloc(newsize);
